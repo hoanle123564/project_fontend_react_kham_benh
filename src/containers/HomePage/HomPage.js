162147f5e736a4fda651c0658edc6d8c1c9fc517
hoanle123564 <prysma123564@gmail.com>
2025-10-04 20:06:07 +0700

    nothing change

--- src/containers/HomePage/HomPage.js
+++ src/containers/HomePage/HomPage.js
@@ -2,15 +2,24 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import HomeHeader from './HomeHeader';
 import Speciality from './Section/Speciality';
-
+import RemoteExam from './Section/RemoteExam';
+import './HomePage.scss'
 
 class HomePage extends Component {
 
     render() {
+        let settings = {
+            dots: false,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 3,
+            slidesToScroll: 2
+        };
         return (
             <>
                 <HomeHeader />
-                <Speciality />
+                <Speciality settings={settings} />
+                <RemoteExam settings={settings} />
             </>
         );
     }
--- src/containers/HomePage/Section/Speciality.js
+++ src/containers/HomePage/Section/Speciality.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'; // kết nối như router
-import './Speciality.scss'
 // Slider 
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
@@ -11,29 +10,21 @@ import CotSong from '../../../assets/khoa/cot-song.png'
 import TaiMuiHong from '../../../assets/khoa/tai-mui-hong.png'
 import TieuHoa from '../../../assets/khoa/tieu-hoa.png'
 import TimMach from '../../../assets/khoa/tim-mach.png'
+
 class Speciality extends Component {
-
-
     render() {
-        let settings = {
-            dots: true,
-            infinite: true,
-            speed: 500,
-            slidesToShow: 3,
-            slidesToScroll: 2
-        };
         return (
             <>
-                <div className='section-specialty'>
-                    <div className='specialty-container'>
-                        <div className='specialty-header'>
+                <div className='section-share section-specialty'>
+                    <div className='section-container'>
+                        <div className='section-header'>
                             <span className='title-section'>Chuyên khoa phổ biến</span>
                             <button className='btn-section'>Xem thêm</button>
 
                         </div>
-                        <div className='specialty-body'>
-                            <Slider {...settings}>
-                                <div className='image-customize   p-2 sm:p-5 border-[1.5px] '>
+                        <div className='section-body'>
+                            <Slider {...this.props.settings}>
+                                <div className='image-customize'>
                                     <img src={ThanKinh} alt=''></img>
                                     <div className='title-img'>Khoa thần kinh</div>
                                 </div>
