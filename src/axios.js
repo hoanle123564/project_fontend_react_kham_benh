d9d5076c8bc25852548a5e25fd7f790a0c24058f
hoanle123564 <prysma123564@gmail.com>
2025-10-30 21:52:54 +0700

    format

--- src/axios.js
+++ src/axios.js
@@ -1,16 +1,13 @@
-import axios from 'axios';
+import axios from "axios";
 
 const instance = axios.create({
-    baseURL: process.env.REACT_APP_BACKEND_URL,
-    // withCredentials: true
+  baseURL: process.env.REACT_APP_BACKEND_URL,
+  // withCredentials: true
 });
 
-
-instance.interceptors.response.use(
-    (response) => {
-        const { data } = response;
-        return response.data;
-    }
-);
+instance.interceptors.response.use((response) => {
+  const { data } = response;
+  return response.data;
+});
 
 export default instance;
