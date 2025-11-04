bec763bd462fc4711c35de8b34ac4404f451d56c
hoanle123564 <prysma123564@gmail.com>
2025-11-04 16:12:34 +0700

    add func enter pass to login

--- src/containers/Auth/Login.js
+++ src/containers/Auth/Login.js
@@ -1,105 +1,131 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 import { push } from "connected-react-router";
 import * as actions from "../../store/actions";
-import './Login.scss';
-// import { FormattedMessage } from 'react-intl';
-import { handleLoginAPI } from '../../services/userService';
-// import { error } from 'ajv/dist/vocabularies/applicator/dependencies';
+import { KeyCodeUtils } from "../../utils";
 
+import "./Login.scss";
+// import { FormattedMessage } from 'react-intl';
+import { handleLoginAPI } from "../../services/userService";
+// import { error } from 'ajv/dist/vocabularies/applicator/dependencies';
 
 class Login extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            email: '',
-            pass: '',
+            email: "",
+            pass: "",
             ShowPass: false,
-            errMessage: ''
-        }
+            errMessage: "",
+        };
     }
     handleOnchangeName = (event) => {
         this.setState({
-            email: event.target.value
-        }
-        )
+            email: event.target.value,
+        });
         console.log(event.target.value);
-
-    }
+    };
 
     handleOnchangePass = (event) => {
         this.setState({
-            pass: event.target.value
-        }
-        )
+            pass: event.target.value,
+        });
         console.log(event.target.value);
+    };
+    // Dùng để enter ngay password là login luôn
+    handlerKeyDown = (event) => {
+        const keyCode = event.which || event.keyCode;
+        if (keyCode === KeyCodeUtils.ENTER) {
+            this.handleOnClick();
 
-    }
-
+        }
+    };
     handleOnClick = async () => {
         this.setState({
-            errMessage: ''
-        })
+            errMessage: "",
+        });
         try {
-            const data = await handleLoginAPI(this.state.email, this.state.pass)
+            const data = await handleLoginAPI(this.state.email, this.state.pass);
             if (data && data.errCode !== 0) {
                 this.setState({
-                    errMessage: data.message
-                })
-
+                    errMessage: data.message,
+                });
             } else {
-                this.props.userLoginSuccess(data.user)
+                this.props.userLoginSuccess(data.user);
                 this.setState({
-                    errMessage: 'Login succeed !'
-                })
+                    errMessage: "Login succeed !",
+                });
             }
         } catch (error) {
-            console.log('error>>', error);
+            console.log("error>>", error);
             this.setState({
-                errMessage: error.response.data.message
-            })
+                errMessage: error.response.data.message,
+            });
         }
-    }
+    };
     handleShowHidePass = () => {
         this.setState({
-            ShowPass: !this.state.ShowPass
-        })
-    }
+            ShowPass: !this.state.ShowPass,
+        });
+    };
     render() {
         return (
             <>
-                <div className='login-background'>
-                    <div className='login-container'>
-                        <div className='login-content row'>
-                            <div className='col-12 text-center text-login'>Login</div>
-                            <div className='col-12 form-group login-input'>
+                <div className="login-background">
+                    <div className="login-container">
+                        <div className="login-content row">
+                            <div className="col-12 text-center text-login">Login</div>
+                            <div className="col-12 form-group login-input">
                                 <label>Username : </label>
-                                <input type='text' value={this.state.email}
-                                    className='form-control' placeholder='Enter your name' onChange={(event) => this.handleOnchangeName(event)}></input>
+                                <input
+                                    type="text"
+                                    value={this.state.email}
+                                    className="form-control"
+                                    placeholder="Enter your name"
+                                    onChange={(event) => this.handleOnchangeName(event)}
+                                ></input>
                             </div>
 
-                            <div className='col-12 form-group login-input'>
+                            <div className="col-12 form-group login-input">
                                 <label>Password : </label>
-                                <div className='custom-input-pass'>
-                                    <input type={this.state.ShowPass ? 'text' : 'password'} value={this.state.pass}
-                                        className='form-control' placeholder='Enter your password' onChange={(event) => this.handleOnchangePass(event)}></input>
-                                    <span onClick={(event) => this.handleShowHidePass()}><i className={this.state.ShowPass ? "fa-solid fa-eye-slash" : "fa-solid fa-eye"}></i></span>
-
+                                <div className="custom-input-pass">
+                                    <input
+                                        type={this.state.ShowPass ? "text" : "password"}
+                                        value={this.state.pass}
+                                        className="form-control"
+                                        placeholder="Enter your password"
+                                        onChange={(event) => this.handleOnchangePass(event)}
+                                        onKeyDown={(event) => this.handlerKeyDown(event)}
+                                    ></input>
+                                    <span onClick={(event) => this.handleShowHidePass()}>
+                                        <i
+                                            className={
+                                                this.state.ShowPass
+                                                    ? "fa-solid fa-eye-slash"
+                                                    : "fa-solid fa-eye"
+                                            }
+                                        ></i>
+                                    </span>
                                 </div>
                             </div>
-                            <div className='col-12 ' style={{ color: 'red' }}>
+                            <div className="col-12 " style={{ color: "red" }}>
                                 {this.state.errMessage}
                             </div>
-                            <div className='col-12 '>
-                                <button className='btn-login' onClick={() => this.handleOnClick()}>Login</button>
+                            <div className="col-12 ">
+                                <button
+                                    className="btn-login"
+                                    onClick={() => this.handleOnClick()}
+                                >
+                                    Login
+                                </button>
                             </div>
-                            <div className='col-12'>
-                                <span className='forgot-password'>Forgot your password ?</span>
+                            <div className="col-12">
+                                <span className="forgot-password">Forgot your password ?</span>
                             </div>
-                            <div className='col-12 text-center'>
-                                <span className='text-center'>Or login with:</span>
+                            <div className="col-12 text-center">
+                                <span className="text-center">Or login with:</span>
                             </div>
-                            <div className='col-12 social-login'>
+                            <div className="col-12 social-login">
                                 <i className="fa-brands fa-facebook-f facebook"></i>
                                 <i className="fa-brands fa-google-plus-g google"></i>
                             </div>
@@ -107,22 +133,23 @@ class Login extends Component {
                     </div>
                 </div>
             </>
-        )
+        );
     }
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
     return {
         lang: state.app.language,
-        userInfo: state.user.userInfo
+        userInfo: state.user.userInfo,
     };
 };
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
     return {
         navigate: (path) => dispatch(push(path)),
         userLoginFail: () => dispatch(actions.userLoginFail()),
-        userLoginSuccess: (userInfor) => dispatch(actions.userLoginSuccess(userInfor))
+        userLoginSuccess: (userInfor) =>
+            dispatch(actions.userLoginSuccess(userInfor)),
     };
 };
 
