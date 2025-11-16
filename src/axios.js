11b9be7de02120f739543bf1c0424c5e7de683e0
hoanle123564 <prysma123564@gmail.com>
2025-11-16 12:03:00 +0700

    add request token  in axios and add package jwt-decode

--- src/axios.js
+++ src/axios.js
@@ -1,13 +1,27 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL,
-  // withCredentials: true
+  baseURL: process.env.REACT_APP_BACKEND_URL || "http://localhost:8080",
 });
 
-instance.interceptors.response.use((response) => {
-  const { data } = response;
-  return response.data;
-});
+// Gửi token lên server trong mỗi request
+instance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+// Luôn trả response.data
+instance.interceptors.response.use(
+  (response) => response.data,
+  (error) => Promise.reject(error)
+);
 
 export default instance;
