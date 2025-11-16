7d50c7ea9cde7d9fe0394c3b665e499dbca41e73
hoanle123564 <prysma123564@gmail.com>
2025-11-16 12:14:58 +0700

    add route for clinic and specialty

--- src/routes/System.js
+++ src/routes/System.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Redirect, Route, Switch } from "react-router-dom";
-import UserManage from "../containers/System/UserManage";
 import TableManageUser from "../containers/System/Admin/TableManageUser";
 import Header from "../containers/Header/Header";
 import ManageDoctor from "../containers/System/Admin/ManageDoctor";
 import ManageSpectialty from "../containers/System/Specialty/ManageSpecialty";
 import ManageClinic from "../containers/System/Clinic/ManageClinic";
+import ManageSchedule from "../containers/System/Doctor/ManageSchedule";
+import AddClinic from "../containers/System/Clinic/AddClinic";
+import EditClinic from "../containers/System/Clinic/EditClinic";
+import AddSpecialty from "../containers/System/Specialty/AddSpecialty";
+import EditSpecialty from "../containers/System/Specialty/EditSpecialty";
 class System extends Component {
   render() {
     const { systemMenuPath, isLoggedIn } = this.props;
@@ -16,12 +20,16 @@ class System extends Component {
         <div className="system-container">
           <div className="system-list">
             <Switch>
-              <Route path="/system/user-manage" component={UserManage} />
-              <Route path="/system/user-redux" component={TableManageUser} />
+
+              <Route path="/system/user-manage" component={TableManageUser} />
               <Route path="/system/manage-doctor" component={ManageDoctor} />
               <Route path="/system/manage-clinic" component={ManageClinic} />
+              <Route path="/system/add-clinic" component={AddClinic} />
+              <Route path="/system/edit-clinic/:id" component={EditClinic} />
+              <Route path="/system/manage-schedule" component={ManageSchedule} />
               <Route path="/system/manage-specialty" component={ManageSpectialty} />
-
+              <Route path="/system/add-specialty" component={AddSpecialty} />
+              <Route path="/system/edit-specialty/:id" component={EditSpecialty} />
               <Route
                 component={() => {
                   return <Redirect to={systemMenuPath} />;
