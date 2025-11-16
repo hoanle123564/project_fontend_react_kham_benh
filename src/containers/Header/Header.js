3861e029ef5fed045ed6799eda83ec71bbac8307
hoanle123564 <prysma123564@gmail.com>
2025-11-16 12:13:08 +0700

    change decentralization to jwt

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -7,55 +7,86 @@ import { adminMenu, doctorMenu } from "./menuApp";
 import "./Header.scss";
 import vietnam from "../../assets/flag/vietnam.png";
 import united from "../../assets/flag/united_kingdom.png";
-import { languages, USER_ROLE } from "../../utils/constant";
-import { FormattedMessage } from "react-intl"; // chuyển đổi ngôn ngữ
+import { languages } from "../../utils/constant";
+import { FormattedMessage } from "react-intl";
 import _ from "lodash";
 import LifeCare from '../../assets/logo3.png';
+import { jwtDecode } from "jwt-decode";
+
 class Header extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      changeLanguage: true,
       menuApp: [],
+      userDisplayName: "",
     };
   }
-  change = (language) => {
-    this.props.changeLangguageAppRedux(language);
-  };
+
   componentDidMount() {
-    let { userInfo } = this.props;
-    let menu = [];
-    if (userInfo && _.isEmpty(userInfo) === false) {
-      let { roleId } = userInfo;
-      if (roleId === USER_ROLE.ADMIN) {
-        menu = adminMenu;
-      }
-      if (roleId === USER_ROLE.DOCTOR) {
-        menu = doctorMenu;
-      }
-      this.setState({
-        menuApp: menu,
-      });
+    this.loadMenuByToken();
+  }
+
+  componentDidUpdate(prevProps) {
+    // Cập nhật menu nếu token thay đổi
+    if (prevProps.token !== this.props.token) {
+      this.loadMenuByToken();
     }
   }
 
+  loadMenuByToken = () => {
+    const token = this.props.token || localStorage.getItem("token");
+
+    if (!token) return;
+
+    let decoded = {};
+    try {
+      decoded = jwtDecode(token);
+    } catch (err) {
+      console.log("Decode JWT failed: ", err);
+      return;
+    }
+
+    let { roleId, firstName, lastName } = decoded;
+    let menu = [];
+
+    if (roleId === "R1") menu = adminMenu;
+    if (roleId === "R2") menu = doctorMenu;
+
+    this.setState({
+      menuApp: menu,
+      userDisplayName: `${firstName || ""} ${lastName || ""}`,
+    });
+  };
+
+  change = (language) => {
+    this.props.changeLangguageAppRedux(language);
+  };
+
+  processLogout = () => {
+    this.props.processLogout();
+    localStorage.removeItem("token");
+  };
+
   render() {
-    const { processLogout, language, userInfo } = this.props;
+    const { language, userInfo } = this.props;
+
     // chọn cờ dựa trên language
     const flagSrc = language === languages.VI ? vietnam : united;
 
     return (
-      <div className="sidebar-container ">
+      <div className="sidebar-container">
+
         {/* Logo */}
         <div className="sidebar-logo">
           <img src={LifeCare} alt="logo" />
         </div>
+
         {/* Menu chính */}
         <div className="sidebar-menu">
           <Navigator menus={this.state.menuApp} />
         </div>
 
-        {/* Footer: ngôn ngữ, chào mừng, logout */}
+        {/* Footer */}
         <div className="sidebar-footer">
           <div className="footer-top">
             <span>
@@ -67,6 +98,8 @@ class Header extends Component {
           </div>
 
           <div className="footer-bottom">
+
+
             <div
               className="flag"
               onClick={() =>
@@ -78,7 +111,8 @@ class Header extends Component {
               <img src={flagSrc} alt="flag" />
               <span>{language === languages.VI ? "VN" : "EN"}</span>
             </div>
-            <div className="logout-btn" onClick={processLogout}>
+
+            <div className="logout-btn" onClick={this.processLogout}>
               <i className="fas fa-sign-out-alt"></i>
             </div>
           </div>
@@ -88,20 +122,16 @@ class Header extends Component {
   }
 }
 
-const mapStateToProps = (state) => {
-  return {
-    isLoggedIn: state.user.isLoggedIn,
-    language: state.app.language,
-    userInfo: state.user.userInfo.user,
-  };
-};
+const mapStateToProps = (state) => ({
+  language: state.app.language,
+  token: state.user.token, // lấy token từ redux
+  userInfo: state.user.userInfo.user,
+});
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    processLogout: () => dispatch(actions.processLogout()),
-    changeLangguageAppRedux: (languages) =>
-      dispatch(actions.changeLangguageApp(languages)),
-  };
-};
+const mapDispatchToProps = (dispatch) => ({
+  processLogout: () => dispatch(actions.processLogout()),
+  changeLangguageAppRedux: (languages) =>
+    dispatch(actions.changeLangguageApp(languages)),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Header);
