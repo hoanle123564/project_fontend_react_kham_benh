4fd8991bd8c12c65adb62d2686bf3603adb44671
hoanle123564 <prysma123564@gmail.com>
2025-11-18 00:55:35 +0700

    add action log out

--- src/store/reducers/patientReducer.js
+++ src/store/reducers/patientReducer.js
@@ -1,12 +1,16 @@
+import actionTypes from '../actions/actionTypes';
+
 const initialState = {
     isLoggedIn: false,
     patientInfo: null,
-    token: null
+    token: null,
+    listAppointment: [],
+
 };
 
 export default function patientReducer(state = initialState, action) {
     switch (action.type) {
-        case 'PATIENT_LOGIN_SUCCESS':
+        case actionTypes.PATIENT_LOGIN_SUCCESS:
             return {
                 ...state,
                 isLoggedIn: true,
@@ -14,8 +18,19 @@ export default function patientReducer(state = initialState, action) {
                 token: action.data.token
             };
 
-        case 'PATIENT_LOGOUT':
+        case actionTypes.PATIENT_LOGOUT:
             return initialState;
+
+        case actionTypes.FETCH_LIST_APPOINTMENT_FOR_PATIENT:
+            return {
+                ...state,
+                listAppointment: action.data,
+            };
+        case actionTypes.FETCH_LIST_APPOINTMENT_FOR_PATIENT_FAIL:
+            return {
+                ...state,
+                listAppointment: [],
+            };
 
         default:
             return state;
